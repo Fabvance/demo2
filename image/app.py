443bc4a77e8959bc1beb0eb56253ce353ea19e78
Fabvance <141661149+Fabvance@users.noreply.github.com>
2023-08-12 11:03:10 -0700

    demo0

---
+++ image/app.py
@@ -0,0 +1,198 @@
+from turtle import width
+from flask import Flask, render_template, request, send_file, send_from_directory
+import os
+import cv2
+import numpy as np
+from io import BytesIO
+import mediapipe as mp
+
+app = Flask(__name__)
+app.secret_key = b'\xec\xe1\xa5nv\x17>\x97\x89%l\xfd\x83\xbc\\z'
+
+mp_face_detection = mp.solutions.face_detection
+mp_drawing = mp.solutions.drawing_utils
+face_detection = mp_face_detection.FaceDetection(min_detection_confidence=0.2)
+
+# Add these lines to import and initialize the face mesh solution
+mp_face_mesh = mp.solutions.face_mesh
+face_mesh = mp_face_mesh.FaceMesh(max_num_faces=1)
+
+
+def calculate_face_tilt(face_landmarks, ih, iw):
+    left_eye = np.array([face_landmarks.landmark[33].x *
+                        iw, face_landmarks.landmark[33].y * ih])
+    right_eye = np.array(
+        [face_landmarks.landmark[263].x * iw, face_landmarks.landmark[263].y * ih])
+    nose_tip = np.array([face_landmarks.landmark[1].x * iw,
+                        face_landmarks.landmark[1].y * ih])
+
+    # Calculate left-right tilt angle
+    eye_angle = np.degrees(np.arctan2(
+        right_eye[1] - left_eye[1], right_eye[0] - left_eye[0]))
+
+    # Calculate up-down tilt angle
+    eye_center = (left_eye + right_eye) / 2
+    nose_angle = np.degrees(np.arctan2(
+        nose_tip[1] - eye_center[1], nose_tip[0] - eye_center[0]))
+
+    return eye_angle, nose_angle
+
+
+def overlay_product(person_img, product_img):
+    with mp_face_detection.FaceDetection(min_detection_confidence=0.2) as face_detection:
+        results = face_detection.process(
+            cv2.cvtColor(person_img, cv2.COLOR_BGR2RGB))
+
+        if results.detections:
+            for detection in results.detections:
+                bboxC = detection.location_data.relative_bounding_box
+                ih, iw, _ = person_img.shape
+                x, y, w, h = int(bboxC.xmin * iw), int(bboxC.ymin *
+                                                       ih), int(bboxC.width * iw), int(bboxC.height * ih)
+
+                top = y - int(h * 0.9)
+                bottom = y + h - int((h/2) * 0.1)
+                left = x - int(w * 0.17)
+                right = x + w + int(w * 0.18)
+
+                width = right - left
+                height = bottom - top
+
+                # Calculate face angles using face landmarks
+                face_landmarks = face_mesh.process(cv2.cvtColor(
+                    person_img, cv2.COLOR_BGR2RGB)).multi_face_landmarks[0]
+                eye_angle, nose_angle = calculate_face_tilt(
+                    face_landmarks, ih, iw)
+
+                # Rotate the product image according to the face angles (negate the eye_angle)
+                M = cv2.getRotationMatrix2D(
+                    (width // 2, height // 2), -eye_angle, 1)
+                product_resized = cv2.resize(product_img, (width, height))
+                product_rotated = cv2.warpAffine(
+                    product_resized, M, (width, height))
+
+                # Adjust the product position based on the up-down tilt angle
+                vertical_shift = int(
+                    height * 0.1 * np.sin(np.radians(nose_angle)))
+                top += vertical_shift
+                bottom += vertical_shift
+
+                if product_rotated.shape[2] == 4:
+                    alpha = product_rotated[:, :, 3] / 255.0
+                    alpha = np.expand_dims(alpha, axis=2)
+
+                    if person_img[top:top + height, left:left + width].shape[:2] == product_rotated[:, :, :3].shape[:2]:
+                        person_img[top:top + height, left:left + width] = (
+                            1 - alpha) * person_img[top:top + height, left:left + width] + alpha * product_rotated[:, :, :3]
+                else:
+                    if person_img[top:top + height, left:left + width].shape == product_rotated.shape:
+                        person_img[top:top + height,
+                                   left:left + width] = product_rotated
+
+    return person_img
+
+
+def overlay_frame(result_img, frame_path, person_img, product_img):
+
+    rotation_angle = 9
+
+    width = height = 135
+
+    rectangle_coords = (17, 310, width, height)
+
+    # Read the frame image with transparency (alpha channel)
+    frame_img = cv2.imread(frame_path, cv2.IMREAD_UNCHANGED)
+
+    # Resize the frame image to match the dimensions of the result image
+    frame_resized = cv2.resize(
+        frame_img, (result_img.shape[1], result_img.shape[0]))
+
+    # Calculate the size of the rectangle where the person_img should be placed
+    rect_x, rect_y, rect_width, rect_height = rectangle_coords
+
+    # Resize the person_img to fit inside the rectangle
+    person_resized = cv2.resize(person_img, (rect_width, rect_height))
+
+    # Get the center of the person image
+    center = (person_resized.shape[1] // 2, person_resized.shape[0] // 2)
+
+    # Calculate the rotation matrix
+    rotation_matrix = cv2.getRotationMatrix2D(center, rotation_angle, 1)
+
+    # Apply the rotation matrix to the person image
+    person_rotated = cv2.warpAffine(
+        person_resized, rotation_matrix, (person_resized.shape[1], person_resized.shape[0]))
+
+    # Place the rotated person_img on the result_img at the specified rectangle coordinates
+    result_img[rect_y:rect_y + rect_height,
+               rect_x:rect_x + rect_width] = person_rotated
+
+    # Overlay the frame image on the result image, taking into account the alpha channel
+    if frame_resized.shape[2] == 4:
+        alpha = frame_resized[:, :, 3] / 255.0
+        alpha = np.expand_dims(alpha, axis=2)
+
+        result_img = (1 - alpha) * result_img + alpha * frame_resized[:, :, :3]
+
+    # Resize the product image to a smaller size
+    # Adjust the size (30, 30) as needed
+    product_resized = cv2.resize(product_img, (40, 40))
+
+    # Choose the position where you want to place the product image on the frame image
+    product_x, product_y = 150, 65  # Adjust the position as needed
+
+    # Overlay the resized product image on the frame image
+    if product_resized.shape[2] == 4:
+        product_alpha = product_resized[:, :, 3] / 255.0
+        product_alpha = np.expand_dims(product_alpha, axis=2)
+
+        result_img[product_y:product_y + product_resized.shape[0],
+                   product_x:product_x + product_resized.shape[1]] = (1 - product_alpha) * result_img[product_y:product_y + product_resized.shape[0], product_x:product_x + product_resized.shape[1]] + product_alpha * product_resized[:, :, :3]
+    else:
+        result_img[product_y:product_y + product_resized.shape[0],
+                   product_x:product_x + product_resized.shape[1]] = product_resized
+    return result_img.astype(np.uint8)
+
+
+@app.route('/overlay', methods=['POST'])
+def overlay():
+    person_img = request.files['person']
+
+    person_img = cv2.imdecode(np.frombuffer(
+        person_img.read(), np.uint8), cv2.IMREAD_COLOR)
+
+    # copy the original image
+    person_copy = person_img.copy()
+
+    # Load the product image from the specified path
+    product_img = cv2.imread('image/cap.png', cv2.IMREAD_UNCHANGED)
+
+    result = overlay_product(person_img, product_img)
+
+    # Call the overlay_frame function to add the frame image
+    frame_path = 'image/frame.png'
+    result_with_frame = overlay_frame(
+        result, frame_path, person_copy, product_img)
+
+    buffer = BytesIO()
+    _, encoded = cv2.imencode(
+        '.jpg', result_with_frame, [int(cv2.IMWRITE_JPEG_QUALITY), 95])
+    buffer.write(encoded)
+
+    buffer.seek(0)
+
+    return send_file(buffer, mimetype='image/jpeg')
+
+
+@app.route('/image/<path:filename>')
+def serve_image(filename):
+    return send_from_directory('image', filename)
+
+
+@app.route('/')
+def index():
+    return send_file('index.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
